ad61ec03fd6c8dce8d2515232765f61ff40106b2
Brian J. Cardiff <bcardiff@gmail.com>
2020-03-16 18:32:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# lokalise-github-integration
+
+TODO: Write a description here
+
+## Installation
+
+TODO: Write installation instructions here
+
+## Usage
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/lokalise-github-integration/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Brian J. Cardiff](https://github.com/your-github-user) - creator and maintainer
---
+++ src/lokalise-github-integration.cr
@@ -0,0 +1,62 @@
+require "kemal"
+
+LOKALISE_X_SECRET = ENV["LOKALISE_X_SECRET"]
+GITHUB_TOKEN      = ENV["GITHUB_TOKEN"]
+GITHUB_ORG_REPO   = ENV["GITHUB_ORG_REPO"]
+DEBOUNCE          = ENV["DEBOUNCE"]?.try(&.to_i) || 10
+
+class Queue
+  @mutex = Mutex.new
+  @requests = Hash(String, Concurrent::Future(Nil)?).new
+
+  def enqueue(lang : String)
+    @mutex.synchronize do
+      @requests[lang]?.try &.cancel
+      @requests[lang] = delay(DEBOUNCE.seconds) { trigger(lang) }
+    end
+  end
+
+  def trigger(lang : String) : Nil
+    @mutex.synchronize do
+      @requests[lang]?.try &.cancel
+      @requests[lang] = nil
+    end
+
+    puts "Triggering #{lang}!"
+
+    HTTP::Client.post("https://api.github.com/repos/#{GITHUB_ORG_REPO}/dispatches",
+      headers: HTTP::Headers{"Authorization" => "token #{GITHUB_TOKEN}"},
+      body: %q({"event_type": "translation-updated", "client_payload": {"lang": "#{lang}"}})) do |response|
+      begin
+        puts response.body_io.gets
+      rescue
+        puts "Unable to show request body https://github.com/crystal-lang/crystal/issues/8461 ?"
+      end
+      puts "Request to GitHub finished"
+    end
+  end
+end
+
+Q = Queue.new
+
+get "/" do
+  render "src/views/index.html.ecr"
+end
+
+post "/trigger" do |env|
+  language = env.params.body["language"].as(String)
+  Q.trigger(language)
+  env.redirect "/"
+end
+
+post "/lokalise" do |env|
+  secret = env.request.headers["X-Secret"]
+  if secret == LOKALISE_X_SECRET
+    language = env.params.json["language"].as(Hash(String, JSON::Any))["iso"].as_s
+    Q.enqueue(language)
+  else
+    puts "Invalid X-Secret header"
+  end
+end
+
+Kemal.run
